de4ab90a0fce42efd158fa5c209cabd0f93f5c91
501 <501@academicos.cesde.edu.co>
2022-01-25 11:13:02 -0500

    actualizacion

---
+++ js/controlador.js
@@ -0,0 +1,5 @@
+//mportacion de elementos
+//import nombreFuncion from nombre de archivo
+import {llenarTienda} from "./llenadoTienda.js"
+
+llenarTienda()
---
+++ js/llenadoTienda.js
@@ -0,0 +1,17 @@
+
+//exportaccion de elementos
+export function llenarTienda(){
+    let productos = [
+        {nombre:"Muñeca Rapunzel",precio:220000,foto:"../img/productos/munecaRapunzel.jpg",decripcion:"Muñeca Rapunzel de 40cm, articulada y puede cantar"},
+        {nombre:"Muñeca Ana",precio:250000,foto:"../img/productos/munecaAna.jpg",decripcion:"Muñeca Ana de 40cm, articulada y puede cantar"},
+        {nombre:"Muñeca Ariel",precio:280000,foto:"../img/productos/munecaAriel.jpg",decripcion:"Muñeca Ariel de 40cm, articulada y puede cantar"},
+        {nombre:"Muñeca Elsa",precio:220000,foto:"../img/productos/munecaElsa.jpg",decripcion:"Muñeca Elsa de 40cm, articulada y puede cantar"},
+        {nombre:"Muñeca coleccion cenicienta",precio:180000,foto:"../img/productos/cenicienta.jpg",decripcion:"Muñeca Coleccion Disney Otoño de 30cm"},
+        {nombre:"Minni Mouse Navideña",precio:300000,foto:"../img/productos/minni.jpg",decripcion:"Muñeca de coleccion Navideña de 30cm"},
+        {nombre:"Peluche Olaf",precio:90000,foto:"../img/productos/pelucheOlaf.jpg",decripcion:"Peluche olaf de la pelicula Frozen, 50cm y de algodon"},
+        {nombre:"Carpa Castillo",precio:120000,foto:"../img/productos/castillo.jpg",decripcion:"Carpa en forma de catillo"},
+        {nombre:"Collar Raya",precio:30000,foto:"../img/productos/collarRaya.jpg",decripcion:"Collar de la pelicula Raya, alumbra en la oscuridad con pilas recargables"},
+        {nombre:"Kit de muñecas Raya",precio:230000,foto:"../img/productos/kitPrincesas.jpg",decripcion:"Set de 3 muñecas de 20 cm de la pelicula raya"},
+    ]
+    console.log(productos)
+}
